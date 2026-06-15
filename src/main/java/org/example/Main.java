49105ea728434c8dd13189d992ec45f23e6c1618
eassae0 <eassae0@yandex.ru>
2026-06-15 12:35:41 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println(i * i);
+            System.out.println(i * i * i * i);
         }
     }
 }
