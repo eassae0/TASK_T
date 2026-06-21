0b7e327effd27effcfda02dcd12b7b8914c060e1
eassae0 <eassae0@yandex.ru>
2026-06-22 01:20:18 +0300

    init

---
+++ src/main/java/Task_5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long left = sc.nextLong();
+        long right = sc.nextLong();
+
+        long length = 0;
+        int count = 0;
+        long num = 0;
+
+
+        while (num <= right) {
+            length = length * 10 + 1;
+            for (int i = 1; i < 10; i++) {
+                num = length * i;
+                if (left <= num && right >= num) count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
