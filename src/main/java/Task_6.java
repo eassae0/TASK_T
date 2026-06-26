fb0c4e23dfca2b1b9b4e4a176ed3fc9ebd7e087a
eassae0 <eassae0@yandex.ru>
2026-06-26 18:17:04 +0300

    fix

--- src/main/java/Task_6.java
+++ src/main/java/Task_6.java
@@ -17,14 +17,21 @@ public class Task_6 {
             if (position % 2 == 1 && height % 2 == 0) badOdd.add(position);
         }
 
-        if (badOdd.size() == 1 && badEven.size() == 1) {
-            int i1 = badOdd.getFirst();
-            int i2 = badEven.getFirst();
+
+        if (badOdd.isEmpty() && badEven.isEmpty()) {
+            if (n >= 3) {
+                System.out.println("1 3");
+            } else System.out.println("-1 -1");
+
+        } else if (badOdd.size() == 1 && badEven.size() == 1) {
+            int i1 = badOdd.get(0);
+            int i2 = badEven.get(0);
             if (i1 < i2) {
                 System.out.println(i1 + " " + i2);
             } else {
                 System.out.println(i2 + " " + i1);
             }
+
         } else {
             System.out.println("-1 -1");
         }
